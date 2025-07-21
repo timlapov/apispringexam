cd0251813ac10acbf1d774eba690ec3554825091
Timothée <83133135+timlapov@users.noreply.github.com>
2025-07-21 13:55:27 +0200

    Initial commit

---
+++ src/main/java/art/lapov/apispringexam/ApispringexamApplication.java
@@ -0,0 +1,13 @@
+package art.lapov.apispringexam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApispringexamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApispringexamApplication.class, args);
+    }
+
+}
---
+++ src/test/java/art/lapov/apispringexam/ApispringexamApplicationTests.java
@@ -0,0 +1,13 @@
+package art.lapov.apispringexam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApispringexamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
